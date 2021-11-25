33efd7e4c3725f1f48173fbcc77249dbd90a51ea
pawel.dubaj <pdubaj@interia.pl>
2021-11-25 14:48:38 +0100

    cucumber raport

--- qagui/src/test/java/pl/jsystems/qa/qagui/bdd/RunTest.java
+++ qagui/src/test/java/pl/jsystems/qa/qagui/bdd/RunTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources",
         glue = "classpath:pl.jsystems.qa.qagui.bdd",
-        plugin = {"pretty", "summary", "html:target/cucumber/report.html", "json: target/cucumber.json",
+        plugin = {"pretty", "summary", "html:target/cucumber/report.html", "json:target/cucumber.json",
         "junit:target/cucumber.xml", "rerun:target/rerun.txt"},
         tags =
                 "@BDD "
