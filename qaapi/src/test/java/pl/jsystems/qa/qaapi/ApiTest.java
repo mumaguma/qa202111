1f430d13deaa4a20f477baf0f628503cf20bf97f
dwi <dominik.witkiewicz@qualityminds.pl>
2021-11-26 10:06:54 +0100

    Api tests added

--- qaapi/src/test/java/pl/jsystems/qa/qaapi/ApiTest.java
+++ qaapi/src/test/java/pl/jsystems/qa/qaapi/ApiTest.java
@@ -1,18 +1,21 @@
 package pl.jsystems.qa.qaapi;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class ApiTest {
 
     @Test
-    public void firstApiTest() {
+    public void firstApiTest1() {
         ValidatableResponse response = RestAssured
                 .given()
                 .get("https://fakerestapi.azurewebsites.net/api/v1/Activities")
@@ -27,4 +30,60 @@ public class ApiTest {
         Object firstArrayElement = response.extract().jsonPath().get("[0].");
         Assertions.assertEquals(((LinkedHashMap) firstArrayElement).size(), 4);
     }
+
+    @Test
+    public void apiTest() {
+        List<Author> author = RestAssured
+                .given()
+                .get("http://fakerestapi.azurewebsites.net/api/v1/Authors")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getList("", Author.class);
+
+        Assertions.assertEquals(author.get(0).getId(), 1);
+    }
+
+    @Test
+    public void authorByIdTest() {
+        Author author = RestAssured
+                .given()
+                .get("http://fakerestapi.azurewebsites.net/api/v1/Authors/{id}", 1)
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getList("", Author.class)
+                .get(0);
+
+        Assertions.assertEquals(author.getId(), 1);
+        Assertions.assertEquals(author.getIdBook(), 1);
+    }
+
+    @Test
+    public void authorByIdWithResponseTest() {
+        Response response = RestAssured
+                .given()
+                .get("http://fakerestapi.azurewebsites.net/api/v1/Authors/{id}", 1)
+                .andReturn();
+
+        Author author = response
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getObject("", Author.class);
+
+        Assertions.assertEquals(response.statusCode(), 200);
+
+        Assertions.assertEquals(author.getId(), 1);
+        Assertions.assertEquals(author.getIdBook(), 1);
+    }
+
+
+
 }
